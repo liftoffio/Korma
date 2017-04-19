8c59db0c8c698c28820739f3d754095dbd553ea5
Caleb Spare <cespare@gmail.com>
2017-04-18 18:37:07 -0700

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject liftoff/korma "0.4.3-liftoff-2"
+(defproject liftoff/korma "0.4.3-liftoff-3"
   :description "Tasty SQL for Clojure"
   :url "http://github.com/korma/Korma"
   :mailing-list {:name "Korma Google Group"
